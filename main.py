0a89ac079a0818eaa21ae41827a0d6fba6fa6507
Yurec28 <karapetyan-y@list.ru>
2024-12-09 23:51:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_completed_remote_sensing = 12
+number_of_hours_spent = 1.5
+name = "Python"
+time_per_task = number_of_hours_spent/number_of_completed_remote_sensing
+print (name, number_of_completed_remote_sensing, number_of_hours_spent, time_per_task)
